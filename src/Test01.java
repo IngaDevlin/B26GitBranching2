a24010510f2e956c90594e11adab5abf6df4ab07
IngaDevlin <98770504+IngaDevlin@users.noreply.github.com>
2022-04-06 20:41:02 -0400

    added loop

--- src/Test01.java
+++ src/Test01.java
@@ -1,5 +1,10 @@
 public class Test01 {
     public static void main(String[] args) {
 
+        for (int i = 0; i < 0; i++) {
+            System.out.println("hi");
+            
+
+        }
     }
 }
